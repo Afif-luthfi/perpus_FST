f9df11a5b6f4cbcc099c14c685c231f011bb8e24
Afif Luthfi <afif.lthf2006@gmail.com>
2026-06-21 23:25:50 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,13 @@
 <?php
-// Mengizinkan akses dari Frontend (CORS)
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_perpus_fst";
+// UBAH 4 BARIS INI SESUAI DATA DARI INFINITYFREE (TAHAP 1)
+$host = "sql102.infinityfree.com"; // Ganti dengan MySQL Host Name
+$user = "if0_42236967";            // Ganti dengan MySQL User Name
+$pass = "4qMisQIkGQZbJmo";    // Ganti dengan Account Password (yang bintang-bintang)
+$db   = "if0_42236967_perpus";     // Ganti dengan MySQL DB Name
 
 $conn = new mysqli($host, $user, $pass, $db);
 
